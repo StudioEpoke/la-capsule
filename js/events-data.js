97e67a0d060364004a4c256fdabfba465d9e822d
Nicolas Lupien <niclupien@gmail.com>
2016-06-07 16:50:07 -0400

    stuff

--- js/events-data.js
+++ js/events-data.js
@@ -4,9 +4,9 @@
 var canvasEvents = {
   {% for film in site.data.films %}
     {% if film.titre2 %}
-    '{{ film.date }}' : '<a href="http://www.imdb.com/title/{{ film.imdb }}" target="_blank">{{ film.titre }}</a><br><a href="http://www.imdb.com/title/{{ film.imdb2 }}" target="_blank">{{ film.titre2 }}</a>',
+    '{{ film.date }}' : '<a href="http://www.imdb.com/title/{{ film.imdb | strip }}" target="_blank">{{ film.titre }}</a><br><a href="http://www.imdb.com/title/{{ film.imdb2 | strip }}" target="_blank">{{ film.titre2 }}</a>',
     {% else %}
-    '{{ film.date }}' : '<a href="http://www.imdb.com/title/{{ film.imdb }}" target="_blank">{{ film.titre }}</a>',
+    '{{ film.date }}' : '<a href="http://www.imdb.com/title/{{ film.imdb | strip }}" target="_blank">{{ film.titre }}</a>',
     {% endif %}
   {% endfor %}
 };
