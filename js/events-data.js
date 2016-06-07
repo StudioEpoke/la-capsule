288b49b52a744a53712a2d5a850137c170b1b1a9
Nicolas Lupien <niclupien@gmail.com>
2016-06-07 15:58:41 -0400

    first commit

---
+++ js/events-data.js
@@ -0,0 +1,11 @@
+var canvasEvents = {
+	'06-03-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>The Shining</a><br><a href="http://www.imdb.com/title/tt0081505/" target=_blank>Room 237</a>',
+	'06-07-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>The Heat</a>',
+	'06-08-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>21 and under</a>',
+	'06-08-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>Compil du festival Spasm</a>',
+	'06-12-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>Pocahontas</a>',
+	'06-13-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>Sport</a>',
+	'06-23-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>PBS Concussion</a>',
+	'06-24-2016' : '<a href="http://www.imdb.com/title/tt0081505/" target=_blank>Men in Black</a>',
+
+};
